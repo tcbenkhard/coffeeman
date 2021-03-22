c222a5ce585821d94a3f81b6f61d80146d04f12f
Timo Benkhard <tcbenkhard@gmail.com>
2021-03-22 16:59:00 +0100

    Add controller data

--- src/main/java/nl/benkhard/coffeeman/features/home/HomeController.java
+++ src/main/java/nl/benkhard/coffeeman/features/home/HomeController.java
@@ -1,16 +1,25 @@
 package nl.benkhard.coffeeman.features.home;
 
+import nl.benkhard.coffeeman.features.customers.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    CustomerRepository customerRepository;
 
     @GetMapping
-    public String home() {
-        return "/fragment/customers/index";
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/fragment/customers/index");
+        modelAndView.addObject("customers", customerRepository.findAll());
+
+        return modelAndView;
     }
 }
